2810f904559e2c47e00e529ad24d44c9c599cf74
chen <gu.che@husky.neu.edu>
2020-05-16 14:12:57 -0400

    2

--- src/day1/BST4.java
+++ src/day1/BST4.java
@@ -1,5 +1,16 @@
 package day1;
 
+//1.3 Given a target integer T and an integer array A sorted in ascending order, find the index of the last occurrence of T in A or return -1 if there is no such index.
+//        Assumptions
+//        There can be duplicate elements in the array.
+//        Examples
+//        A = {1, 2, 3}, T = 2, return 1
+//        A = {1, 2, 3}, T = 4, return -1
+//        A = {1, 2, 2, 2, 3}, T = 2, return 3
+//        Corner Cases
+//        What if A is null or A is array of zero length? We should return -1 in this case.
+
+
 public class BST4 {
 
     int bst4(int[] array, int target){
