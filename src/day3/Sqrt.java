ce694cca8dac829892ffd926e33f51cf9fc5c6ad
chen <gu.che@husky.neu.edu>
2020-05-17 19:51:54 -0400

    day3

---
+++ src/day3/Sqrt.java
@@ -0,0 +1,35 @@
+package day3;
+
+public class Sqrt {
+
+    // 2.1
+//     Implement double sqrt(double x) and x >= 0.
+// * 	Compute and return the square root of x.
+// * Example:
+// * 	Given n = 2 return 1.41421356
+
+    public double sqrt(double x){
+        double left = 0.0;
+        double right= x;
+        if(x  == 1){
+            return 1;
+        }
+            double mid = left +(right-left)/2;
+            while(true){
+                if(mid * mid - x <= 0.001){
+                    break;
+                }
+                if(mid * mid > x){
+                    right  = mid;
+                }
+                else{
+                    left = mid;
+                }
+            }
+            return mid;
+
+    }
+
+
+
+}
