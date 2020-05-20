a3041111067537f9dd7b783df42895c93aab35cb
chen <gu.che@husky.neu.edu>
2020-05-20 00:25:08 -0400

    day5

--- src/day5/SubsetWithDuplicates.java
+++ src/day5/SubsetWithDuplicates.java
@@ -1,9 +1,9 @@
-package day4;
+package day5;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Subsets {
+public class SubsetWithDuplicates {
 
     // 1. How many states do we have -> (how many level do we need)
 // 2. How many different states should we try to put on each level
@@ -16,30 +16,6 @@ public class Subsets {
 //             3          /    \      /  \    /  \    / \
 //                      [1,2,3][1,2][1,3][1] [23][2] [3] []
 
-
-    public static List<String> subSet(int[] array){
-        List<String> result = new ArrayList<String>();
-        if(array == null || array.length == 0){
-            return result;
-        }
-        StringBuilder sb = new StringBuilder();
-        dfs(array, sb, 0, result);
-        return result;
-    }
-    private static void dfs(int[] array, StringBuilder sb, int level, List<String> result){
-        // base case
-        if(level == array.length){
-            result.add(sb.toString());
-            return;
-        }
-        // recursion rules
-        // case 1.0 do not pick
-
-        dfs(array, sb, level + 1, result);
-        // case 1.1 pick the corresponding int
-        dfs(array, sb.append(array[level]), level + 1, result);
-        sb.deleteCharAt(sb.length() - 1);
-    }
 
     // if contains duplicates:
     // [1,1,2,3,]
@@ -96,6 +72,5 @@ public class Subsets {
 
         System.out.println(result);
     }
-
 
 }
