42954f4b3c1a8429192a7f1cf8701c153be88c94
chen <guchennn@gmail.com>
2020-06-17 00:03:02 -0400

    day18

--- src/day17/KthSmallestMatrix.java
+++ src/day17/KthSmallestMatrix.java
@@ -1,5 +1,8 @@
 package day17;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class KthSmallestMatrix {
     //Given a matrix of size N x M. For each row the elements are sorted in ascending order, and for each column the elements are also sorted in ascending order. Find the Kth smallest number in it.
     //
@@ -34,7 +37,7 @@ public class KthSmallestMatrix {
     //    marker, priorityqueue
 
     // Binary Search(for its value)
-    public int kthSmallest(int[][] matrix, int k){
+    public int kthSmallest0(int[][] matrix, int k){
         int m = matrix.length;
         int n = matrix[0].length;
         int left = matrix[0][0], right = matrix[m][n];
@@ -50,4 +53,114 @@ public class KthSmallestMatrix {
         }
         return left;
     }
+
+    // bfs2 通常而言是有按一定规律进行方向的搜索的，通常只有两个方向。
+    public  int kthsmallest2(int[][] matrix, int k){
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return -1;
+
+        }
+        int[][] direction = {{1,0},{0,-1}};
+        int column = matrix.length ;
+        int row = matrix[0].length;
+        boolean[][] visited = new boolean[column][row];
+
+        PriorityQueue<Point> heap = new PriorityQueue<>(new Comparator<Point>(){
+            @Override
+            public int compare(Point p1, Point p2){
+                if(p1.value == p2.value){
+                    return 0;
+                }
+                return p1.value < p2.value ? -1:1;
+            }
+
+        });
+        heap.offer(new Point(0,0,matrix[0][0]));
+        visited[0][0] = false;
+        for(int j = 0; j < k-1; j++){
+            Point p = heap.poll();
+            for(int i = 0; i< direction.length; i++){
+                int x = p.x+ direction[i][0];
+                int y = p.y+ direction[i][1];
+                if(x > 0 && x < column && y > 0 && y< row && visited[x][y]){
+                    heap.offer(new Point(x,y,matrix[x][y]));
+                    visited[x][y] = false;
+                }
+            }
+
+        }
+        Point result= heap.peek();
+        return result.value;
+    }
+
+    public int kthSmallest(int[][] matrix, int k){
+        if(matrix == null || matrix.length == 0 || matrix[0].length  == 0 || k < 0){
+            return -1;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        // int byte boolean long short char -> Integer, Boolean, Long, Char
+
+        PriorityQueue<Point> minHeap = new PriorityQueue<Point>(k, new Comparator<Point>(){
+            @Override
+            public int compare(Point c1, Point c2){
+
+                if(c1.value == c2.value){
+                    return 0;
+                }
+                return c1.value < c2.value ? -1:1;
+            }
+        });
+        boolean[][] visited = new boolean[rows][cols];
+        minHeap.offer(new Point(0,0,matrix[0][0]));
+        visited[0][0] = true;
+        for(int i = 0; i < k - 1; i++){
+            Point cur = minHeap.poll();
+            if(cur.x + 1 < rows && !visited[cur.x + 1][cur.y]){
+                minHeap.offer(new Point(cur.x + 1, cur.y, matrix[cur.x + 1][cur.y]));
+                visited[cur.x+1][cur.y] = true;
+            }
+            if(cur.y + 1 < cols && !visited[cur.x][cur.y + 1]){
+                minHeap.offer(new Point(cur.x, cur.y + 1, matrix[cur.x][cur.y + 1]));
+                visited[cur.x][cur.y+1] = true;
+            }
+
+        }
+        return minHeap.peek().value;
+    }
+
+
+
+//     while(!queue.isEmpty())
+//     1 2 3 4 5
+//     3 4 5 6 7
+//     4 5 6 7 8
+//     used              [1,2,3,3,4, 4,4,5,5]
+//     priority queue    [,4,4,5,5]
+
+
+
+    class Point{
+        int x, y,value;
+        Point(int x, int y, int value){
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+    }
+    public static void main(String[] args) {
+        int[][] matrix1 = {{1,3,5,7},{2,4,8,9},{3,6,11,15},{6,8,13,18}};
+        KthSmallestMatrix k1 = new KthSmallestMatrix();
+//        System.out.println("expected 5th = 4, actual =  " + k1.kthsmallest2(matrix1,5));
+//        System.out.println("expected 8th = 6, actual =  " + k1.kthsmallest2(matrix1,8));
+        System.out.println("expected 5th = 4, actual =  " + k1.kthSmallest(matrix1,5));
+        System.out.println("expected 8th = 6, actual =  " + k1.kthSmallest(matrix1,8));
+//        System.out.println("expected 5th = 4, actual =  " + k1.kthsmallest2(matrix1,5));
+//        System.out.println("expected 8th = 6, actual =  " + k1.kthsmallest2(matrix1,8));
+//        System.out.println("expected 5th = 4, actual =  " + k1.kthSmallest0(matrix1,5));
+//        System.out.println("expected 8th = 6, actual =  " + k1.kthSmallest0(matrix1,8));
+    }
+
+
+
 }
---
+++ src/day18/kthSmallestPoly.java
@@ -0,0 +1,69 @@
+package day18;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class kthSmallestPoly {
+//// Find the Kth smallest number s such that s = 3 ^ x * 5 ^ y * 7 ^ z, x > 0 and y > 0 and z > 0, x, y, z are all integers.
+//
+//// Assumptions
+//
+//// K >= 1
+//// Examples
+//
+//// the smallest is 3 * 5 * 7 = 105
+//// the 2nd smallest is 3 ^ 2 * 5 * 7 = 315
+//// the 3rd smallest is 3 * 5 ^ 2 * 7 = 525
+//// the 5th smallest is 3 ^ 3 * 5 * 7 = 945
+public int kth(int k){
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+    Set<Integer> visited = new HashSet<>();
+    minHeap.offer(3*5*7);
+    visited.add(3*5*7);
+    while(k > 1){
+        int cur = minHeap.poll();
+        if(visited.add(3*cur)){
+            minHeap.offer(3*cur);
+        }
+        if(visited.add(5*cur)){
+            minHeap.offer(5*cur);
+        }
+        if(visited.add(7*cur)){
+            minHeap.offer(7*cur);
+        }
+        k--;
+    }
+    return minHeap.peek();
+}
+
+    // PriorityQueue, Collection.sort implements Comparable or Comparator
+    // Time : k^3
+    public int kthSmllestPoly(int k){
+        PriorityQueue<Integer> polynomial = new PriorityQueue<>();
+        int cur = 3*5*7;
+        polynomial.offer(cur);
+        Set<Integer> dup = new HashSet<>();
+        for(int i = 1;  i < k-1; i++){
+            cur = polynomial.poll();
+            dup.add(cur);
+            if(!dup.contains(cur*3)){
+                polynomial.offer(cur*3);
+                dup.add(cur*3);
+            }
+            if(!dup.contains(cur*5)){
+                polynomial.offer(cur*5);
+                dup.add(cur*5);
+            }
+            if(!dup.contains(cur*5)){
+                polynomial.offer(cur*7);
+                dup.add(cur*7);
+            }
+
+        }
+        return polynomial.peek();
+
+    }
+
+
+}
