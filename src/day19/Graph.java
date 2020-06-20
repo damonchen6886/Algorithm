11067b691ee37aaf9a89145b565dea90e2f2db56
chen <guchennn@gmail.com>
2020-06-20 16:31:47 -0400

    updates

---
+++ src/day19/Graph.java
@@ -0,0 +1,33 @@
+package day19;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+
+    // Implement graph by adjcentlist, undirected and unweighted
+    class UnweightedGraphNode{
+        //adjenctlist
+        int val;
+        List<UnweightedGraphNode> neighbors;
+        UnweightedGraphNode(int val){
+            this.val = val;
+            neighbors = new ArrayList<>();
+        }
+    }
+
+    // Implement graph by adjacentlist, directed and weighted
+    class WeightGraphNode{
+        //adjenctlist
+        int val;
+        Map<WeightGraphNode, Integer> neighbors;
+        WeightGraphNode(int val){
+            this.val = val;
+            neighbors = new HashMap<>();
+        }
+    }
+
+
+}
---
+++ src/day19/IsBipartite.java
@@ -0,0 +1,21 @@
+package day19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IsBipartite {
+    public boolean isBipartite(List<GraphNode> graph){
+
+    }
+    class GraphNode{
+        //adjenctlist
+        int val;
+        List<GraphNode> neighbors;
+        GraphNode(int val){
+            this.val = val;
+            neighbors = new ArrayList<>();
+        }
+    }
+
+
+}
