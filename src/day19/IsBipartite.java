32bbc513f892da3c5ca16fee296d8bdc4579e5f5
chen <guchennn@gmail.com>
2020-06-23 00:41:41 -0400

    day20

--- src/day19/IsBipartite.java
+++ src/day19/IsBipartite.java
@@ -1,7 +1,6 @@
 package day19;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 //Determine if an undirected graph is bipartite. A bipartite graph is one in which the nodes can be divided into two groups such that no nodes have direct edges to other nodes in the same group.
@@ -29,8 +28,46 @@ import java.util.List;
 //The graph is represented by a list of nodes, and the list of nodes is not null.
 public class IsBipartite {
     public boolean isBipartite(List<GraphNode> graph){
-        return false;
-
+        // Assumption
+        if(graph == null || graph.size() == 0){
+            return true;
+        }
+        // key: node  value: which group
+        HashMap<GraphNode, Integer> visited = new HashMap<GraphNode, Integer>();
+        // do not use index
+        for(GraphNode node: graph){ // itertor
+            if(!BFS(node, visited)){
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean BFS(GraphNode node, HashMap<GraphNode, Integer> visited){
+        // if this node has been traversed before, do not traverse it again
+        if(visited.containsKey(node)){
+            return true;
+        }
+        // start bfs here
+        Queue<GraphNode> queue = new LinkedList<GraphNode>();
+        queue.offer(node);
+        visited.put(node, 0);
+        while(!queue.isEmpty()){
+            GraphNode cur = queue.poll();
+            int curGroup = visited.get(cur); // 0 : 1
+            int neiGroup = curGroup == 0 ? 1 : 0;
+            for(GraphNode nei : cur.neighbors){
+                // case 1: if the neighbor has not been visited
+                if(!visited.containsKey(nei)){
+                    visited.put(nei, neiGroup);
+                    queue.offer(nei);
+                } else if(visited.get(nei) != neiGroup){
+                    // case 2: it the neighbor has been visited before
+                    return false;
+                }
+            }
+        }
+        // case 3: has been visited before and visited.get(nei) == neiGroup
+        return true;
     }
     class GraphNode{
         //adjenctlist
