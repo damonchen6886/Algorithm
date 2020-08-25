46e2a9266f69f40e78d9eef7702c7695a3ff20b1
damonchen6886 <guchennn@gmail.com>
2020-08-24 23:53:55 -0400

    day40

---
+++ src/day41/LFUCache.java
@@ -0,0 +1,125 @@
+package day41;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LFUCache<K, V>{
+
+    static class Node<K, V>{
+        Node<K, V> next;
+        Node<K, V> prev;
+        int frequency;
+        K key;
+        V value;
+
+        Node(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+        void update(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int limit;
+    private Node<K, V> head; // latest recent
+    private Node<K, V> tail; // oldest
+    private Map<K, Node<K, V>> map;
+
+    public LFUCache(int limit){
+        this.limit = limit;
+        map = new HashMap<>();
+    }
+
+
+
+    public void set(K key, V value){
+        Node<K,V> cur = new Node(key, value);
+        if(!map.containsKey(key)){
+            insert(cur);
+            map.put(key,cur);
+            return;
+
+        }
+        if(map.containsKey(key)){
+            cur = map.get(key);
+            cur.frequency++;
+            swap(cur);
+            head = cur.next == null? cur : cur.next;
+        }
+        return;
+
+    }
+
+    public int get(K key){
+        if(map.get(key) == null){
+            return 0;
+        }
+        Node<K,V> cur =  map.get(key);
+        cur.frequency+= 1;
+        swap(cur);
+        return cur.frequency;
+    }
+
+
+    private void swap(Node<K,V> cur){
+        if(cur.next == null){
+            return;
+        }
+        while(cur.next.frequency == cur.frequency){
+
+            Node<K,V> temp = cur.next.next;
+            cur.next.next= cur;
+            cur.prev = temp;
+            cur.next = temp.next;
+            temp.next =cur;
+        }
+        return;
+
+    }
+
+    private void insert(Node<K,V> cur){
+        Node<K,V> before = findBefore(cur);
+        Node<K,V> after = findAfter(cur);
+        if(cur.prev == null){
+            cur.next.prev = cur;
+            cur =head;
+            return;
+        }
+        if(cur.next == null){
+            before.next =cur;
+            cur.prev = before;
+            cur = tail;
+        }
+
+        else{
+            cur.next = after;
+            cur.prev = before;
+            before.next = cur;
+            after.prev = cur;
+
+        }
+        return;
+    }
+    private Node<K,V> findBefore(Node<K,V> cur){
+        while(head!= null && head.next != null){
+            if(head.value == cur.value && head.next.value != cur){
+                return head.prev;
+            }
+
+        }
+        return null;
+    }
+
+    private Node<K,V> findAfter(Node<K,V> cur){
+        while(head!= null && head.next != null){
+            if(head.value == cur.value && head.next.value != cur){
+                return head.next;
+            }
+        }
+        return null;
+    }
+
+}
+
---
+++ src/day41/LRUCache.java
@@ -0,0 +1,196 @@
+package day41;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    class Node{
+        int val;
+        int key;
+        Node prev;
+        Node next;
+        public Node(int val, int key){
+            this.val = val;
+            this.key = key;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> map = new HashMap<Integer, Node>();
+    // adjusable
+    private int lastKey;
+    private int head;
+
+    public LRUCache(int capacity){
+        this.capacity = capacity;
+    }
+
+    public int get(int key){
+        if(!map.containsKey(key)){
+            return -1;
+        }
+        Node temp = map.get(key);
+        set(key, temp.val);
+        return temp.val;
+    }
+
+    public void set(int key, int value){
+        Node cur = new Node(value,key);
+        if(map.isEmpty()){
+            map.put(key, cur);
+            lastKey = key;
+            head = key;
+            return;
+        }
+        if(!map.containsKey(key)){
+            if(map.size() < capacity){
+                Node lastNode = map.get(lastKey);
+                lastNode.next = cur;
+                map.put(key,cur);
+                lastKey = key;
+                return;
+            }
+            else{
+                Node topNode = map.get(head);
+                Node nextNode = topNode.next;
+                // update 2nd node pointer
+                nextNode.prev = cur;
+                map.put(key, cur);
+                // remove 1st element in map;
+                map.remove(head);
+                head = key;
+                return;
+            }
+        }
+        if(map.containsKey(key)){
+            if(map.size() == 1 || key == lastKey){
+                return;
+            }
+            if( key == head){
+                Node topNode = map.get(head);
+                Node nextNode = topNode.next;
+                // update 2nd node pointer
+                nextNode.prev = null;
+                Node lastNode  = map.get(lastKey);
+                lastNode.next = cur;
+                map.put(key, cur);
+                // remove 1st element in map;
+                map.remove(head);
+                head = nextNode.val;
+                lastKey = key;
+                return;
+            }
+            // remove existing key
+            Node before = map.get(key).prev;
+            Node after = map.get(key).next;
+            before.next = after;
+            after.prev = before;
+            map.remove(key);
+            // add key to the end;
+            Node lastNode = map.get(lastKey);
+            lastNode.next = cur;
+            map.put(key,cur);
+            lastKey =key;
+            return;
+        }
+        return;
+    }
+    //-------------------------------------------
+
+
+    public  class LRUCache2<K, V>{
+
+
+        // Implement for double linked list
+         class Node<K, V>{
+            Node<K, V> next;
+            Node<K, V> prev;
+            K key;
+            V value;
+
+            Node(K key, V value){
+                this.key = key;
+                this.value = value;
+            }
+            void update(K key, V value){
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private final int limit;
+        private Node<K, V> head; // latest recent
+        private Node<K, V> tail; // oldest
+        private Map<K, Node<K, V>> map;
+
+        // constructor
+        public LRUCache2(int limit){
+            this.limit = limit;
+            this.map = new HashMap<>();
+        }
+
+        public void set(K key, V value){
+            Node<K, V> node = null;
+            // case 1: contains key
+            if(map.containsKey(key)){
+                node = map.get(key);
+                node.value = value;
+                remove(node);
+            } else if(map.size() < limit){
+                // case 2: do not contains key and the size is smaller than limit
+                node = new Node<K, V>(key, value);
+            } else {
+                node = tail;
+                remove(node);
+                node.update(key, value);
+            }
+            append(node);
+        }
+
+        public V get(K key){
+            Node<K,V> node = map.get(key);
+            if(node == null){
+                return null;
+            }
+            // update to the latest recent key and value
+            remove(node);
+            append(node);
+            return node.value;
+        }
+
+        private Node<K, V> append(Node<K, V> node){
+            map.put(node.key, node);
+            // case 1: if it is the first node
+            if(head == null){
+                head = node;
+            } else {
+                node.next = head;
+                head.prev = node;
+                head = node;
+            }
+            return node;
+        }
+
+        private Node<K, V> remove(Node<K, V> node){
+            map.remove(node.key);
+            if(node.prev != null){
+                node.prev.next = node.next;
+            }
+            if(node.next != null){
+                node.next.prev = node.prev;
+            }
+            // Maintain the head and the tail
+            if(node == head){
+                head = head.next;
+            }
+            if(node == tail){
+                tail = tail.prev;
+            }
+            node.next = node.prev = null;
+            return node;
+        }
+
+    }
+
+}
