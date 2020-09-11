ce6c7c43243bb5affe70729e41ce4004eb006862
damonchen6886 <guchennn@gmail.com>
2020-09-10 21:50:40 -0400

    minor updates

--- src/day32/isValidTree.java
+++ src/day32/isValidTree.java
@@ -2,6 +2,7 @@ package day32;
 
 import java.util.*;
 
+// leetcode 261: Graph Valid Tree
 public class isValidTree {
 
     //Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
