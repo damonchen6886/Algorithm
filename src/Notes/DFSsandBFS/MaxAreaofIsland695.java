1abbddb4926f737c17d66236dda37c2b80cb809c
damonchen6886 <guchennn@gmail.com>
2020-10-21 23:04:28 -0400

    updates

--- src/Notes/DFSsandBFS/MaxAreaofIsland695.java
+++ src/Notes/DFSsandBFS/MaxAreaofIsland695.java
@@ -1,6 +1,8 @@
 package Notes.DFSsandBFS;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 public class MaxAreaofIsland695 {
@@ -76,6 +78,109 @@ public class MaxAreaofIsland695 {
         curMax[0]++;
         for(int[] dir: direction){
             dfs(grid,x+dir[0],y+dir[1],column,row,curMax);
+        }
+    }
+
+    // Union Find:
+    public int maxAreaOfIsland3(int[][] grid) {
+        if(grid == null || grid.length ==0 || grid[0].length ==0){
+            return 0;
+        }
+        int rows = grid.length;
+        int cols = grid[0].length;
+        UnionFind uf = new UnionFind(grid);
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 1){
+                    if(i > 0 && grid[i-1][j] == 1){
+                        uf.connect(i * cols + j, (i-1)*cols + j);
+                    }
+                    if(j > 0 && grid[i][j-1] == 1){
+                        uf.connect(i * cols + j, (i)*cols + j - 1);
+                    }
+                    if(i < rows - 1 && grid[i + 1][j] == 1){
+                        uf.connect(i * cols + j, (i + 1) * cols + j);
+                    }
+                    if(j < cols - 1 && grid[i][j + 1] == 1){
+                        uf.connect(i * cols + j, (i) * cols + j + 1);
+                    }
+                }
+            }
+        }
+        return uf.query();
+    }
+
+    class UnionFind{
+        private int[] father = null;
+        private int max;
+        int cols;
+        int rows;
+        int[][] grid;
+        // constructor
+        public UnionFind(int[][] g){
+            this.grid = g;
+            rows =  g.length  ;
+            cols = g[0].length ;
+            int n = rows * cols;
+            father = new int[n];
+            for(int i = 0; i < n; i++){
+                father[i] = i;
+            }
+        }
+
+        // find, compressed
+        private int find(int x){
+            // recursively find you dad
+            if(father[x] == x){
+                return x;
+            }
+            // recursive rule
+            return father[x] = find(father[x]);
+        }
+        // connect
+        private void connect(int a, int b){
+            int father_a = find(a);
+            int father_b = find(b);
+            if(father_a != father_b){
+                if(father_a >= father_b){
+                    father[father_a] = father_b;
+                }
+                else{
+                    father[father_b] = father_a;
+                }
+            }
+        }
+
+        private void rebalance(){
+            for(int i = 0; i < rows * cols; i++){
+                find(i);
+            }
+        }
+        private int query(){
+            rebalance();
+            Map<Integer, Integer> map = new HashMap<>();
+            int count = 0;
+            for(int i = 0; i< father.length; i++){
+                map.put(father[i], map.getOrDefault(father[i],0)+1);
+
+            }
+
+            int max =0;
+            for(Map.Entry<Integer, Integer> m: map.entrySet()){
+                max = Math.max(max, m.getValue());
+
+            }
+            if(max ==1){
+                for(int i =0; i < rows; i++){
+                    for(int j = 0; j < cols; j++){
+                        if(grid[i][j] == 1){
+                            return 1;
+                        }
+                    }
+                }
+                return 0;
+            }
+            return max;
         }
     }
 }
