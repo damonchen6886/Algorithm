3c81f646695ee032b83a8bc09395a4eeff876439
damonchen6886 <guchennn@gmail.com>
2021-05-29 18:21:14 -0400

    updates

--- src/DynamicProgramming/ConcatenateWord.java
+++ src/DynamicProgramming/ConcatenateWord.java
@@ -1,7 +1,6 @@
 package DynamicProgramming;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class ConcatenateWord {
     //Given a word and a dictionary, determine if it can be composed by concatenating words from the given dictionary.
@@ -35,6 +34,42 @@ public class ConcatenateWord {
             }
         }
         return dp[dp.length-1];
+    }
+    public boolean canbreak2(String input, List<String> dict){
+        Set<String> set = new HashSet<>(dict);
+        Queue<Integer> q= new ArrayDeque<>();
+        q.offer(0);
+        boolean[] visited= new boolean[input.length()];
+        while(!q.isEmpty()){
+            int start = q.poll();
+            if(!visited[start]){
+                for(int end = start+1; end <= input.length(); end++){
+                    if(set.contains(input.substring(start,end))){
+                        q.offer(end);
+                        if(end == input.length()){
+                            return true;
+                        }
+                    }
+                }
+            }
+            visited[start] = true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        List<String> dict = new ArrayList<>();
+        dict.add("apple");
+        dict.add("pen");
+        List<String> dict2 = new ArrayList<>();
+        dict.add("cats");
+        dict.add("dog");
+        dict.add("sand");
+        dict.add("and");
+        dict.add("cat");
+        ConcatenateWord c = new ConcatenateWord();
+        System.out.println(c.canbreak2("applepenapple", dict));
+        System.out.println(c.canbreak2("catsandog", dict2));
     }
 
 }
