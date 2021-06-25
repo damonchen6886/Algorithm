ae44f1e6073e8a5c746dc20abd1c72754abb6bcb
damonchen6886 <guchennn@gmail.com>
2021-06-24 22:57:45 -0400

    updates

--- src/DynamicProgramming/WordBreak.java
+++ src/DynamicProgramming/WordBreak.java
@@ -3,47 +3,97 @@ package DynamicProgramming;
 import java.util.*;
 
 public class WordBreak {
-//Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
-//
-//Note that the same word in the dictionary may be reused multiple times in the segmentation.
-    //Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
-    //Output: ["cats and dog","cat sand dog"]
+    // leetcode 139
+    //Given a word and a dictionary, determine if it can be composed by concatenating words from the given dictionary.
+    //
+    //Assumptions
+    //
+    //The given word is not null and is not empty
+    //The given dictionary is not null and is not empty and all the words in the dictionary are not null or empty
+    //Examples
+    //
+    //Dictionary: {“bob”, “cat”, “rob”}
+    //
+    //Word: “robob” return false
+    //
+    //Word: “robcatbob” return true since it can be composed by "rob", "cat", "bob"
 
-//    Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
-//    Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
-//    Explanation: Note that you are allowed to reuse a dictionary word.
-public List<String> wordBreak(String s, List<String> wordDict) {
-    Set<String> set = new HashSet<>(wordDict);
-    Map<String, List<String>> map =new HashMap<>();
-    return dfs(map, s, set);
-}
-
-    private List<String> dfs(Map<String, List<String>> map, String s, Set<String> set){
-        if(map.containsKey(s)){
-            return map.get(s);
+    // standard dp
+    public boolean canBreak(String input, String[] dict){
+        Set<String> d = new HashSet<>();
+        for(String s:dict){
+            d.add(s);
         }
-        List<String>result = new ArrayList<>();
-        if(set.contains(s)){
-            result.add(s);
-        }
-        for(int i = 1; i< s.length();i++){
-            String head = s.substring(0,i);
-            if(set.contains(head)){
-                String rest = s.substring(i);
-                List<String> subList = dfs(map, rest,set);
-                for(String cur: subList){
-                    result.add(head + " " + cur);
+        boolean[] dp = new boolean[input.length()+1];
+        dp[0] = true;
+        for(int i = 1; i <= input.length(); i++){
+            for(int j = 0; j < i; j++){
+                if(d.contains(input.substring(j, i)) && dp[j]){
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-        map.put(s,result);
-        return result;
+        return dp[dp.length-1];
+    }
+
+    //bfs
+    public boolean canbreak2(String input, List<String> dict){
+        Set<String> set = new HashSet<>(dict);
+        Queue<Integer> q= new ArrayDeque<>();
+        q.offer(0);
+        boolean[] visited= new boolean[input.length()];
+        while(!q.isEmpty()){
+            int start = q.poll();
+            if(!visited[start]){
+                for(int end = start+1; end <= input.length(); end++){
+                    if(set.contains(input.substring(start,end))){
+                        q.offer(end);
+                        if(end == input.length()){
+                            return true;
+                        }
+                    }
+                }
+            }
+            visited[start] = true;
+        }
+        return false;
+    }
+
+    // dfs with dp
+    public boolean canbreak3(String input, List<String> dict){
+        Set<String> set= new HashSet<>(dict);
+        boolean[] dp = new boolean[input.length()+1];
+        return dfs(input, set, dp, 0);
+    }
+    private boolean dfs(String input, Set<String> set, boolean[] dp, int start){
+        if(start == input.length()){
+            return true;
+        }
+        if(dp[start]){
+            return dp[start];
+        }
+        for(int end = start+1; end<= input.length();end++){
+            if(set.contains(input.substring(start, end))&& dfs(input, set,dp,end)){
+                return dp[start] = true;
+            }
+        }
+        return dp[start]  =false;
     }
 
     public static void main(String[] args) {
-        List<String> l1 = new ArrayList(List.of("cat","cats","and","sand","dog"));
-        WordBreak w = new WordBreak();
-        System.out.println(w.wordBreak("catsanddog",l1));
+        List<String> dict = new ArrayList<>();
+        dict.add("apple");
+        dict.add("pen");
+        List<String> dict2 = new ArrayList<>();
+        dict.add("cats");
+        dict.add("dog");
+        dict.add("sand");
+        dict.add("and");
+        dict.add("cat");
+        WordBreak c = new WordBreak();
+        System.out.println(c.canbreak3("applepenapple", dict));
+        System.out.println(c.canbreak3("catsandog", dict2));
     }
 
 }
