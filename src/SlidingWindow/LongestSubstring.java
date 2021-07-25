b438a9c83258caf24ce0903d09853fe12e85d2b6
damonchen6886 <guchennn@gmail.com>
2021-07-25 14:12:08 -0400

    updates

--- src/SlidingWindow/LongestSubstring.java
+++ src/SlidingWindow/LongestSubstring.java
@@ -1,4 +1,4 @@
-package day39;
+package SlidingWindow;
 
 import java.util.HashSet;
 import java.util.Set;
--- src/SlidingWindow/minWindow.java
+++ src/SlidingWindow/minWindow.java
@@ -1,4 +1,4 @@
-package day39;
+package SlidingWindow;
 
 import java.util.HashMap;
 import java.util.Map;
